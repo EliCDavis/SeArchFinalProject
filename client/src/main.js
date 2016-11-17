e5f1b8a8daf0c79154e0b967c0523343c5dd2f22
Hunter Holder <Blond-in-Blue@users.noreply.github.com>
2016-11-16 22:34:22 -0600

    Secure User Login and Registration (#2)
    * Add secure login
    
    - All functionality should be in place
    - Client needs to be configured
    
    * Change bcrypt-nodejs to bcryptjs
    
    * Clean up and clarify html
    
    - Log out button and tradier should only be in home.html
    - Added comments to clarify functionality
    
    * Client functionality
    
    - Client now works correctly

--- client/src/main.js
+++ client/src/main.js
@@ -2,10 +2,65 @@ var angular = require('angular');
 
 require('angular-material');
 require('rx-angular');
+require('angular-route');
 
-var app = angular.module('TradeNet', ['ngMaterial', 'rx'])
+var app = angular.module('TradeNet', ['ngMaterial', 'rx', 'ngRoute'])
 
 require('./services');
 require('./directives');
+require('./controllers');
+console.log("Before routing");
+app.config(function($routeProvider) {
+
+    console.log("route: ", $routeProvider);
+
+    $routeProvider
+        .when('/', {
+            templateUrl: 'partial/home.html',
+            access: {
+                restricted: true
+            }
+        })
+        .when('/login', {
+            templateUrl: 'partial/login.html',
+            controller: 'loginController',
+            access: {
+                restricted: false
+            }
+        })
+        .when('/logout', {
+            controller: 'logoutController',
+            access: {
+                restricted: true
+            }
+        })
+        .when('/register', {
+            templateUrl: 'partial/register.html',
+            controller: 'registerController',
+            access: {
+                restricted: false
+            }
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+});
+
+
+app.run(function ($rootScope, $location, $route, AuthService) {
+  $rootScope.$on('$routeChangeStart',
+    function (event, next, current) {
+      AuthService.getUserStatus()
+      .then(function(){
+        if (next.access.restricted && !AuthService.isLoggedIn()){
+          $location.path('/login');
+          $route.reload();
+        }
+      });
+  });
+});
+
+
+
 
 angular.bootstrap(document, ['TradeNet']);
