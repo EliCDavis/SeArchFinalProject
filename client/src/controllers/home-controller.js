a3e8758cbb52d663e0eff04bd504d6c333a78a1e
Eli Davis <eli.davis1995@gmail.com>
2016-11-19 20:57:03 -0500

    Market page only displays when it has results

--- client/src/controllers/home-controller.js
+++ client/src/controllers/home-controller.js
@@ -8,10 +8,32 @@ function homeController($scope, $location, Server) {
 
     $scope.lastSearced = "";
 
+    $scope.showSymbolInfo$ = Server.tradierSymbol$.map(function(symbol){
+        return symbol.quotes.unmatched_symbols === undefined;
+    }).merge(Server.lastSearched$.map(function(d){
+        return false;
+    })).startWith(false);
+
+    $scope.showSymbolFailure$ = Server.tradierSymbol$.map(function(symbol){
+        return symbol.quotes.unmatched_symbols !== undefined;
+    }).merge(Server.lastSearched$.map(function(d){
+        return false;
+    })).startWith(false);
+
+    $scope.currentlySearching$ = Server.tradierSymbol$.map(function(symbol){
+        return false;
+    }).merge(Server.lastSearched$.map(function(d){
+        return true;
+    })).startWith(false);
+
     $scope.search = function() {
         var query = $scope.searchQuery.toUpperCase();
         Server.search(query);
         $scope.lastSearced = query;
     };
+
+    Server.tradierSymbol$.subscribe(function(symbol){
+        console.log("Symbol: ", symbol);
+    });
 
 };
